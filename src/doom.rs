cfabf1b18b9f2d3c4a97875e2944b95e342806ca
Linus <linus@cosmos-ink.net>
2021-09-18 20:40:26 +0200

    Remove uneccessary thread safety

--- src/doom.rs
+++ src/doom.rs
@@ -1,13 +1,13 @@
 use lazy_static::lazy_static;
+use std::cell::RefCell;
 use std::convert::TryFrom;
 use std::ffi::CStr;
 use std::os::raw;
-use std::sync::Mutex;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-const DOOMGENERIC_RESX: usize = 640;
-const DOOMGENERIC_RESY: usize = 480;
+pub const DOOMGENERIC_RESX: usize = 640;
+pub const DOOMGENERIC_RESY: usize = 480;
 
 #[derive(Debug, Clone, Copy)]
 pub struct KeyData {
@@ -15,7 +15,7 @@ pub struct KeyData {
     pub key: u8,
 }
 
-pub trait Doom: Send + Sync {
+pub trait Doom {
     fn draw_frame(&mut self, screen_buffer: &[u32], xres: usize, yres: usize);
     fn get_key(&mut self) -> Option<KeyData>;
     fn set_window_title(&mut self, title: &str);
@@ -29,11 +29,11 @@ extern "C" {
 #[no_mangle]
 static mut DG_ScreenBuffer: *const u32 = std::ptr::null();
 //static DG_ScreenBuffer: &[u32] = &[0u32; DOOMGENERIC_RESX * DOOMGENERIC_RESY];
+static mut SCREEN_BUFFER: RefCell<Option<Box<[u32]>>> = RefCell::new(None);
+static mut DOOM_HANDLER: RefCell<Option<Box<dyn Doom>>> = RefCell::new(None);
 
 lazy_static! {
     static ref START_TIME: Instant = Instant::now();
-    static ref SCREEN_BUFFER: Mutex<Option<Box<[u32]>>> = Mutex::new(None);
-    static ref DOOM_HANDLER: Mutex<Option<Box<dyn Doom>>> = Mutex::new(None);
 }
 
 #[no_mangle]
@@ -41,16 +41,16 @@ extern "C" fn DG_Init() {
     println!("DG_Init()");
     println!("Passed millis: {}", DG_GetTicksMs());
 
-    *SCREEN_BUFFER.lock().unwrap() = Some(Box::new([0u32; DOOMGENERIC_RESX * DOOMGENERIC_RESY]));
     unsafe {
+        *SCREEN_BUFFER.get_mut() = Some(Box::new([0u32; DOOMGENERIC_RESX * DOOMGENERIC_RESY]));
         // Setting DG_ScreenBuffer to where the new buffer is
-        DG_ScreenBuffer = SCREEN_BUFFER.lock().unwrap().as_ref().unwrap().as_ptr();
+        DG_ScreenBuffer = SCREEN_BUFFER.get_mut().as_ref().unwrap().as_ptr();
     }
 }
 
 #[no_mangle]
 extern "C" fn DG_GetKey(pressed: *mut raw::c_int, key: *mut raw::c_uchar) -> raw::c_int {
-    let has_key = if let Some(doom_box) = DOOM_HANDLER.lock().unwrap().as_mut() {
+    let has_key = if let Some(doom_box) = unsafe { DOOM_HANDLER.get_mut().as_mut() } {
         if let Some(keydata) = doom_box.get_key() {
             unsafe {
                 // Not tested yet!
@@ -86,8 +86,8 @@ extern "C" fn DG_SleepMs(ms: u32) {
 #[no_mangle]
 extern "C" fn DG_DrawFrame() {
     println!("DG_DrawFrame()");
-    if let Some(doom_box) = DOOM_HANDLER.lock().unwrap().as_mut() {
-        if let Some(screen_buffer) = SCREEN_BUFFER.lock().unwrap().as_mut() {
+    if let Some(doom_box) = unsafe { DOOM_HANDLER.get_mut() }.as_mut() {
+        if let Some(screen_buffer) = unsafe { SCREEN_BUFFER.get_mut() }.as_mut() {
             doom_box.draw_frame(screen_buffer, DOOMGENERIC_RESX, DOOMGENERIC_RESY);
         }
     }
@@ -99,14 +99,14 @@ extern "C" fn DG_SetWindowTitle(title: *const raw::c_char) {
         .to_str()
         .expect("Can't convert title c string to rust string");
     println!("DG_SetWindowTitle({:?})", title);
-    if let Some(doom_box) = DOOM_HANDLER.lock().unwrap().as_mut() {
+    if let Some(doom_box) = unsafe { DOOM_HANDLER.get_mut() }.as_mut() {
         doom_box.set_window_title(title);
     }
 }
 
 pub fn init(doom_impl: impl Doom + 'static) {
     unsafe {
-        *DOOM_HANDLER.lock().unwrap() = Some(Box::new(doom_impl));
+        *DOOM_HANDLER.get_mut() = Some(Box::new(doom_impl));
 
         M_FindResponseFile();
         DG_Init();
